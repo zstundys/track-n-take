a316e82532a51d843bc8325e5d41b237a5b1bc37
Žanas Stundys <zanass@wix.com>
2025-03-27 23:54:40 +0200

    release

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,3 +1,3 @@
 // Auto-generated file. Do not edit manually.
-export const APP_VERSION = '0.0.5';
-export const APP_VERSION_DATE = 1743111922712;
+export const APP_VERSION = '0.0.6';
+export const APP_VERSION_DATE = 1743112461015;
